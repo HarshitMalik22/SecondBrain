5d51a8d5cae98baba5298eaf4a3d2891af2747c7
Harshit <harshitmalik29@gmail.com>
2025-08-08 07:52:36 +0530

    basic auth setup

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import mongoose from "mongoose";
 import jwt from "jsonwebtoken";
 import { UserModel } from "./db.js";
 
+const JWT_PASSWORD = "!2323";
+
 const app = express();
 app.use(express.json());
 
@@ -10,7 +12,8 @@ app.post("/api/v1/signup", async (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
 
-    await UserModel.create({
+    try{
+        await UserModel.create({
         username: username,
         password: password
     })
@@ -18,10 +21,34 @@ app.post("/api/v1/signup", async (req, res) => {
     res.json({
         message: "User signed up"
     })
+    } 
+    catch(e) {
+    res.status(411).json({
+        message: "User already exists"
+    })
+}
 })
 
-app.post("/api/v1/signin", (req, res) => {
-    
+app.post("/api/v1/signin", async (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    const existingUser = await UserModel.findOne({
+        username,
+        password
+    })
+    if (existingUser){
+        const token = jwt.sign({
+            id: existingUser._id,
+        }, JWT_PASSWORD)
+
+        res.json({
+            token
+        })
+    } else{
+        res.status(403).json({
+            message:"Invalid credentials"
+        })
+    }
 })
 
 app.post("/app/ve/content", (req, res) => {
